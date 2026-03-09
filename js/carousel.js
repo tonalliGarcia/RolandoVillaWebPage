0ef777df3eedf1e033416ec650ea70fbb2455a79
tonalliGarcia <tonalligarcia1994@gmail.com>
2026-03-09 17:38:31 -0600

    Actualización del sitio

---
+++ js/carousel.js
@@ -0,0 +1,27 @@
+// Configuración del Carrusel de Swiper
+const swiperDestacados = new Swiper(".swiper-destacados", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    coverflowEffect: {
+      rotate: 15,
+      stretch: 0,
+      depth: 200,
+      modifier: 1,
+      slideShadows: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+});
